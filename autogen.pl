8062341f9ee275ac64a4cb62587fd20ce5afc972
Joshua Hursey <jhursey@us.ibm.com>
2016-05-06 14:37:10 -0500

    Now try to break the build... Still

--- autogen.pl
+++ autogen.pl
@@ -1041,8 +1041,8 @@ sub in_tarball {
 
 # JJH
 #print "This patch should not break the build\n";
-print "This patch should break the build...\n";
-exit -10;
+print "This patch should break the build... Still!!!\n";
+exit -11;
 
 ##############################################################################
 ##############################################################################
