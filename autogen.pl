f6be19726894125e8f39071992fc1695700f7383
Joshua Hursey <jhursey@us.ibm.com>
2016-05-18 21:45:31 -0500

    Testing. success...

--- autogen.pl
+++ autogen.pl
@@ -1040,7 +1040,7 @@ sub in_tarball {
 }
 
 # JJH
-print "This patch should not break the build!!! Maybe... Maybe!!!!\n";
+print "This patch should not break the build!!! Maybe... Maybe!..\n";
 #print "This patch should break the build... Still!!!!...!!...!!\n";
 #exit -17;
 
